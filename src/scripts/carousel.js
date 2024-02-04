b502b9824ad1d60ac489b0a10a288fbacaebbc81
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-04 10:22:37 +0100

    Modify the script so it uses the testimiials retrieved in the page instead of hard coded array.

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,58 +1,15 @@
-const slides = [
-  {
-    title: 'Maria Jones 1',
-    subTitle: 'CEO, Co-Founder, XYZ Inc.',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ut nesciunt temporibus qui sit harum officia sint, soluta, cum, ea quas accusantium ab deserunt consectetur quibusdam nisi aliquam sed mollitia!',
-    img: {
-      url: '/src/images/person-1.jpg',
-      alt: 'Picture of Maria Jones',
-    },
-  },
-  {
-    title: 'Maria Jones 2',
-    subTitle: 'CEO, Co-Founder, XYZ Inc.',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ut nesciunt temporibus qui sit harum officia sint, soluta, cum, ea quas accusantium ab deserunt consectetur quibusdam nisi aliquam sed mollitia!',
-    img: {
-      url: '/src/images/person-1.jpg',
-      alt: 'Picture of Maria Jones',
-    },
-  },
-  {
-    title: 'Maria Jones 3',
-    subTitle: 'CEO, Co-Founder, XYZ Inc.',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ut nesciunt temporibus qui sit harum officia sint, soluta, cum, ea quas accusantium ab deserunt consectetur quibusdam nisi aliquam sed mollitia!',
-    img: {
-      url: '/src/images/person-1.jpg',
-      alt: 'Picture of Maria Jones',
-    },
-  },
-];
+const slides = document.querySelectorAll('.testimonial-item');
 
-// Set the first slide as active
+// Hide all slides
+slides.forEach((slide) => {
+  slide.classList.add('hidden');
+})
+
 let currentIndex = 0;
 let activeSlide = slides[currentIndex];
-const activeItem = document.querySelector('.testimonial-item');
-
-function displayActiveItem() {
-  const slideText = document.createElement('p');
-  slideText.textContent = activeSlide.text;
-
-  const slideTitle = document.createElement('h3');
-  slideTitle.textContent = activeSlide.title;
-
-  const slideSubTitle = document.createElement('p');
-  slideSubTitle.textContent = activeSlide.subTitle;
-
-  const slideImage = document.createElement('img');
-  slideImage.classList.add('slide-image');
-  slideImage.src = activeSlide.img.url;
-  slideImage.alt = activeSlide.img.alt;
-
-  activeItem.append(slideText, slideImage, slideTitle, slideSubTitle);
-}
 
 function nextSlide() {
-  if (currentIndex === 2) {
+  if (currentIndex === (slides.length - 1)) {
     currentIndex = 0;
   } else currentIndex = currentIndex + 1;
 
@@ -61,29 +18,28 @@ function nextSlide() {
 
 function previousSlide() {
   if (currentIndex === 0) {
-    currentIndex = 2
+    currentIndex = slides.length - 1
   } else currentIndex = currentIndex - 1;
 
   return currentIndex;
 }
-console.log(currentIndex)
-displayActiveItem();
+
+activeSlide.classList.toggle('hidden');
 
 const next = document.querySelector('#next');
 
 next.addEventListener('click', () => {
-  activeItem.innerHTML = '';
+  activeSlide.classList.toggle('hidden');
   currentIndex = nextSlide();
-  console.log(currentIndex)
   activeSlide = slides[currentIndex];
-  displayActiveItem();
+  activeSlide.classList.toggle('hidden');
 })
 
 const previous = document.querySelector('#previous');
 
 previous.addEventListener('click', () => {
-  activeItem.innerHTML = '';
+  activeSlide.classList.toggle('hidden');
   currentIndex = previousSlide();
   activeSlide = slides[currentIndex];
-  displayActiveItem();
+  activeSlide.classList.toggle('hidden');
 })
