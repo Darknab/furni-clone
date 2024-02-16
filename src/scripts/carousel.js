8b0921161eb02241430d38895cfcabf5aee0c82f
Darknab <117382045+Darknab@users.noreply.github.com>
2024-02-16 18:40:00 +0100

    Handle carousel animation and prevent JavaScript error on the console on pages that don't display the carousel.

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -24,22 +24,57 @@ function previousSlide() {
   return currentIndex;
 }
 
-activeSlide.classList.toggle('hidden');
-
 const next = document.querySelector('#next');
-
-next.addEventListener('click', () => {
-  activeSlide.classList.toggle('hidden');
-  currentIndex = nextSlide();
-  activeSlide = slides[currentIndex];
-  activeSlide.classList.toggle('hidden');
-})
 
 const previous = document.querySelector('#previous');
 
-previous.addEventListener('click', () => {
+if (activeSlide) {
   activeSlide.classList.toggle('hidden');
-  currentIndex = previousSlide();
-  activeSlide = slides[currentIndex];
-  activeSlide.classList.toggle('hidden');
-})
+
+  next.addEventListener('click', () => {
+    hideToRight(activeSlide);
+    currentIndex = nextSlide();
+    activeSlide = slides[currentIndex];
+    activeSlide.classList.toggle('hidden');
+    showToRight(activeSlide);
+  });
+
+  previous.addEventListener('click', () => {
+    hideToleft(activeSlide);
+    currentIndex = previousSlide();
+    activeSlide = slides[currentIndex];
+    activeSlide.classList.toggle('hidden');
+    showToleft(activeSlide);
+  })  
+}
+
+
+
+
+const animationList = ['show-to-right', 'show-to-left', 'hide-to-right', 'hide-to-left'];
+
+function hideToRight(slide) {
+  slide.classList.remove(...animationList);
+  slide.classList.add('hide-to-right');
+  slide.addEventListener('animationend', () => {
+    slide.classList.toggle('hidden');
+  }, { once: true });
+}
+
+function showToRight(slide) {
+  slide.classList.remove(...animationList);
+  slide.classList.add('show-to-right');
+}
+
+function hideToleft(slide) {
+  slide.classList.remove(...animationList);
+  slide.classList.add('hide-to-left');
+  slide.addEventListener('animationend', () => {
+    slide.classList.toggle('hidden');
+  }, {once: true});
+}
+
+function showToleft(slide) {
+  slide.classList.remove(...animationList);
+  slide.classList.add('show-to-left');
+}
